49d2345c4370041e4aa0c716094b6584c3881ce4
Andrew Wise <andrew@andrewwise.co.uk>
2020-03-02 12:43:21 +0000

    Rewrites now include collections

--- rewrites.php
+++ rewrites.php
@@ -17,14 +17,36 @@ $export = array(array(
     'Redirect to'
 ));
 
+$redirects = array();
+
 foreach ($data as $row) {
     $redirect_from = $row['Parent Product Url'];
     $url = explode('/', $row['Parent Product Url']);
     $handle = trim( end( $url ) );
     $redirect_to = "/products/" . $handle;
 
+    $categories = explode(',', $row['Categories']);
+    $categories_parts = explode('>', reset( $categories ));
+
+    array_pop($url);
+    $last_uri = null;
+
+    foreach ($categories_parts as $index => $category) {
+        if (isset($url[$index + 1])) {
+            $redirect_from_category = $last_uri . "/" . $url[$index + 1];
+            $redirects[$redirect_from_category] = "/collections/" . preg_replace("/[^\w]+/", "-", strtolower(trim($category)));
+            $last_uri = $redirect_from_category;
+        }
+    }
+
+    if (!isset($redirects[$redirect_from])) {
+        $redirects[$redirect_from] = $redirect_to;
+    }
+}
+
+foreach ($redirects as $redirect_from => $redirect_to) {
     $export[] = array(
-        $row['Parent Product Url'], // Redirct from
+        $redirect_from, // Redirct from
         $redirect_to, // Redirect to
     );
 }
